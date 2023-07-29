6c2c3db6a4fbabe93f8a2d3603f3f26ae45750ab
saet0sy <saet0sy@gmail.com>
2023-07-29 21:30:27 +0600

    feat: added pagination and headers

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+products = []
+
+for i in range(5):
+    link = f'https://shikimori.me/animes/page/{5}/'
+    page = requests.get(link, headers={'User-Agent': 'Mozilla/5.0'})
+    
+    if page.status_code != 200:
+        print(f"Failed to fetch data from {link}. Status code: {page.status_code}")
+        continue
+    
+    soup = BeautifulSoup(page.text, 'lxml')
+
+    titles_raw = soup.find_all('span', class_='name-en')
+
+    titles = [title.get_text(strip=True) for title in titles_raw]
+
+    for i in range(len(titles)):
+        products.append({
+            'title': titles[i]
+        })
+
+with open('products.json', 'w', encoding='utf-8') as f:
+    json.dump(products, f, ensure_ascii=False, indent=2)
+
+print("Scraping and JSON dumping completed.")
